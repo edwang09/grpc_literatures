b39dbec8a207897beea612267a4211e22e505d05
Jianan Wang <edwang09@gmail.com>
2021-05-03 19:43:01 -0400

    production

--- gRPCserver/config/config.js
+++ gRPCserver/config/config.js
@@ -2,6 +2,6 @@ module.exports = {
     MYSQL_IP:process.env.MYSQL_IP || "mysql_db",
     MYSQL_PORT:process.env.MYSQL_PORT || 3306,
     MYSQL_USER:process.env.MYSQL_USER || "user",
-    MYSQL_PASSWORD:process.env.MYSQL_PASSWORD || "password",
+    MYSQL_ROOT_PASSWORD:process.env.MYSQL_ROOT_PASSWORD || "password",
 
 }
